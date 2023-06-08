4acff942752ea9f1b1b2ea30c2fee3a5a998cb6a
Berdnaski <berdnaski1177@gmail.com>
2023-06-08 18:54:36 -0300

    calculator

---
+++ calculator/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Calculator from './components/Calculator'
+
+function App() {
+  return (
+    <div className="App">
+      <Calculator></Calculator>
+    </div>
+  );
+}
+
+export default App;
